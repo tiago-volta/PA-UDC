47df6d6e70830901ec4eec4c0e048bfd8c14d781
sarasalgueiro <sara.salgueiro.rendo@udc.es>
2026-03-19 16:12:21 +0100

    [VARIOS]Añadida implementación del servicio en la capa modelo.

---
+++ backend/src/main/java/es/udc/paproject/backend/model/services/CinemaServiceImpl.java
@@ -0,0 +1,9 @@
+package es.udc.paproject.backend.model.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CinemaServiceImpl implements CinemaService {
+}
