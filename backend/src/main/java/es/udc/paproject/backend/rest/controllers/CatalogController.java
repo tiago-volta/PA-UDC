e4ff2ed7f16c282b16148ac5c3372fc532a8e6a7
sarasalgueiro <sara.salgueiro.rendo@udc.es>
2026-03-19 17:10:04 +0100

    [FUNC-1] Añadida e implementada visualizar cartelera en CatalogController.

--- backend/src/main/java/es/udc/paproject/backend/rest/controllers/CatalogController.java
+++ backend/src/main/java/es/udc/paproject/backend/rest/controllers/CatalogController.java
@@ -1,9 +1,19 @@
 package es.udc.paproject.backend.rest.controllers;
 
+import es.udc.paproject.backend.model.exceptions.InvalidDateException;
 import es.udc.paproject.backend.model.services.CinemaService;
+import es.udc.paproject.backend.rest.dtos.MovieSessionsConversor;
+import es.udc.paproject.backend.rest.dtos.MovieSessionsDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
@@ -11,4 +21,21 @@ public class CatalogController {
 
     @Autowired
     private CinemaService cinemaService;
+
+    @GetMapping("/movies")
+    @Transactional(readOnly = true)
+    public List<MovieSessionsDto> getBilboard(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date)
+            throws InvalidDateException {
+
+        LocalDate searchDate;
+
+        if (date != null) {
+            searchDate = date;
+        } else {
+            searchDate = LocalDate.now();
+        }
+
+        return MovieSessionsConversor.toMovieSessionsDtos(cinemaService.findCartelera(searchDate));
+    }
 }
