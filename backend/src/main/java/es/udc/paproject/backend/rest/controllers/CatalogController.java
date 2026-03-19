1812afa87eed70fb1146b2721db478557731c9fb
sarasalgueiro <sara.salgueiro.rendo@udc.es>
2026-03-19 17:10:52 +0100

    [FUNC-2] Añadida e implementada visualizar la información detallada de una película en CatalogController.

--- backend/src/main/java/es/udc/paproject/backend/rest/controllers/CatalogController.java
+++ backend/src/main/java/es/udc/paproject/backend/rest/controllers/CatalogController.java
@@ -1,16 +1,16 @@
 package es.udc.paproject.backend.rest.controllers;
 
+import es.udc.paproject.backend.model.exceptions.InstanceNotFoundException;
 import es.udc.paproject.backend.model.exceptions.InvalidDateException;
 import es.udc.paproject.backend.model.services.CinemaService;
+import es.udc.paproject.backend.rest.dtos.MovieConversor;
+import es.udc.paproject.backend.rest.dtos.MovieDto;
 import es.udc.paproject.backend.rest.dtos.MovieSessionsConversor;
 import es.udc.paproject.backend.rest.dtos.MovieSessionsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -37,5 +37,11 @@ public class CatalogController {
         }
 
         return MovieSessionsConversor.toMovieSessionsDtos(cinemaService.findCartelera(searchDate));
+    }
+
+    @GetMapping("/movies/{id}")
+    @Transactional(readOnly = true)
+    public MovieDto findMovie(@PathVariable Long id) throws InstanceNotFoundException {
+        return MovieConversor.toMovieDto(cinemaService.findMovieById(id));
     }
 }
