f9968e0db66b6243251abda4a143bc66b683e159
pablojhd05 <pablo.herrero.diaz@udc.es>
2026-03-20 12:38:25 +0100

    [FUNC-4] Añadida e implementada comprar entradas en CatallogController

--- backend/src/main/java/es/udc/paproject/backend/rest/controllers/CatalogController.java
+++ backend/src/main/java/es/udc/paproject/backend/rest/controllers/CatalogController.java
@@ -2,21 +2,25 @@ package es.udc.paproject.backend.rest.controllers;
 
 import static es.udc.paproject.backend.rest.dtos.SessionConversor.toSessionDto;
 
+import es.udc.paproject.backend.model.entities.Purchase;
 import es.udc.paproject.backend.model.exceptions.InstanceNotFoundException;
 import es.udc.paproject.backend.model.exceptions.InvalidDateException;
 import es.udc.paproject.backend.model.exceptions.SessionAlreadyStartedException;
+import es.udc.paproject.backend.model.exceptions.NotEnoughSeatsException;
 import es.udc.paproject.backend.model.services.CinemaService;
 import es.udc.paproject.backend.rest.dtos.SessionDto;
 import es.udc.paproject.backend.rest.dtos.MovieConversor;
 import es.udc.paproject.backend.rest.dtos.MovieDto;
 import es.udc.paproject.backend.rest.dtos.MovieSessionsConversor;
 import es.udc.paproject.backend.rest.dtos.MovieSessionsDto;
-
+import es.udc.paproject.backend.rest.dtos.BuyTicketsParamsDto;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -56,5 +60,16 @@ public class CatalogController {
     @Transactional(readOnly = true)
     public SessionDto getSession(@PathVariable Long id) throws InstanceNotFoundException,SessionAlreadyStartedException {
         return toSessionDto(cinemaService.findSession(id));
+    }
+
+    @PostMapping("/sessions/{sessionId}/buyTickets")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long buyTickets(@RequestAttribute Long userId, @PathVariable Long sessionId,
+                           @Validated @RequestBody BuyTicketsParamsDto params)
+            throws InstanceNotFoundException, NotEnoughSeatsException, SessionAlreadyStartedException {
+
+        Purchase purchase = cinemaService.buyTickets(userId, sessionId, params.getNumTickets(), params.getBankCard());
+
+        return purchase.getId();
     }
 }
