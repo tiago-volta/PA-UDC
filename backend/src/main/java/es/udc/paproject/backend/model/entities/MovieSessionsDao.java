2279f53a5a4b77644c54f440eebdf9dce80a498e
sarasalgueiro <sara.salgueiro.rendo@udc.es>
2026-03-20 16:52:19 +0100

    [FUNC-1] Eliminado MovieSessionsDao

--- backend/src/main/java/es/udc/paproject/backend/model/entities/MovieSessionsDao.java
+++
@@ -1,7 +0,0 @@
-package es.udc.paproject.backend.model.entities;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface MovieSessionsDao extends CrudRepository<MovieSessions, Long> {
-}
