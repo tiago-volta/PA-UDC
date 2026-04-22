f16061d2b3391b77fb5f9f2194cf525f80e8f775
sarasalgueiro <sara.salgueiro.rendo@udc.es>
2026-04-22 12:05:21 +0200

    [FUNC-1] Añadido selector de día en el frontend

--- frontend/src/modules/catalog/components/Billboard.jsx
+++ frontend/src/modules/catalog/components/Billboard.jsx
@@ -1,14 +1,20 @@
 import {useSelector} from 'react-redux';
 import Movies from './Movies';
 import * as selectors from '../selectors';
+import DateSelector from  "./DateSelector";
 
 const Billboard = () => {
     const movies = useSelector(selectors.getMovies);
 
     return (
-        <Movies movies={movies}/>
+        <div>
+            <DateSelector id="billboardDate" className="mb-2 w-auto"/>
+            <Movies movies={movies}/>
+        </div>
+
+
     );
 
 }
 
-export default Billboard; 
+export default Billboard;
