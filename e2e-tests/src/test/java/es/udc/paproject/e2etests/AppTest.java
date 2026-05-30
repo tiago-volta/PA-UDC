802717c81794b744880487c969d00f04f345abd6
pablojhd05 <pablo.herrero.diaz@udc.es>
2026-05-31 00:23:36 +0200

    [TT-0] Creación del proyecto Maven e2e-tests y ejemplo Selenium

---
+++ e2e-tests/src/test/java/es/udc/paproject/e2etests/AppTest.java
@@ -0,0 +1,52 @@
+package es.udc.paproject.e2etests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+
+	WebDriver driver;
+
+	@BeforeEach
+	public void setup() {
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void eightComponents() {
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+		String title = driver.getTitle();
+		assertEquals("Web form", title);
+
+		WebElement textBox = driver.findElement(By.name("my-text"));
+		WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+		textBox.sendKeys("Selenium");
+		submitButton.click();
+
+		WebElement message = driver.findElement(By.id("message"));
+		String value = message.getText();
+		assertEquals("Received!", value);
+
+	}
+
+	@AfterEach
+	public final void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+}
